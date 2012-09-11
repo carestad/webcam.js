d085719a90e1f746b2b827d6ed8f2b9103d6676b
Alexander Karlstad <alexander.karlstad@gmail.com>
2012-09-11 22:05:32 +0200

    Found out it was better to have the onclick event listener inside the hasUserMedia() check. You know, just in case of an emergency.

--- webcam.js
+++ webcam.js
@@ -3,8 +3,7 @@ var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 var img = document.querySelector('img');
 var link = document.createElement('a');
-
-function hasUserMedia() {
+var hasUserMedia = function() {
     if (!navigator.getUserMedia) {
         navigator.getUserMedia = navigator.webkitGetUserMedia ||
             navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -14,6 +13,7 @@ function hasUserMedia() {
     }
     return !!(navigator.getUserMedia);
 }
+
 if (hasUserMedia()) {
     var failure = function(e) {
         console.log("Faaaaaaaail", e);
@@ -28,22 +28,22 @@ if (hasUserMedia()) {
     }
 
     navigator.getUserMedia({video:true}, success, failure);
-}
 
-video.addEventListener('click', function() {
-    var width = this.videoWidth;
-    var height = this.videoHeight;
-    canvas.width = width;
-    canvas.height = height;
-    // draw webcam picture in canvas
-    ctx.drawImage(video, 0, 0);
-    // create data URL and insert into <img/>
-    img.src = canvas.toDataURL('image/png');
-    // download picture when clicking on it
-    img.onclick = function() {
-        // set filename
-        link.setAttribute('download', 'webcam-'+location.hostname+'-'+Date.now()+'.png');
-        link.href = canvas.toDataURL('image/png');
-        link.click();
-    };
-}, false);
+    video.addEventListener('click', function() {
+        var width = this.videoWidth;
+        var height = this.videoHeight;
+        canvas.width = width;
+        canvas.height = height;
+        // draw webcam picture in canvas
+        ctx.drawImage(video, 0, 0);
+        // create data URL and insert into <img/>
+        img.src = canvas.toDataURL('image/png');
+        // download picture when clicking on it
+        img.onclick = function() {
+            // set filename
+            link.setAttribute('download', 'webcam-'+location.hostname+'-'+Date.now()+'.png');
+            link.href = canvas.toDataURL('image/png');
+            link.click();
+        };
+    }, false);
+}
