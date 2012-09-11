8e23fb36581bfc46677774fc73c933a16d87c2c0
Alexander Karlstad <alexander.karlstad@gmail.com>
2012-09-11 22:03:43 +0200

    Added the code. See? It's right here.

---
+++ webcam.js
@@ -0,0 +1,49 @@
+var video = document.querySelector('video');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var img = document.querySelector('img');
+var link = document.createElement('a');
+
+function hasUserMedia() {
+    if (!navigator.getUserMedia) {
+        navigator.getUserMedia = navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    }
+    if (!window.URL) {
+        window.URL = window.webkitURL || window.mozURL;
+    }
+    return !!(navigator.getUserMedia);
+}
+if (hasUserMedia()) {
+    var failure = function(e) {
+        console.log("Faaaaaaaail", e);
+    };
+
+    var success = function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+
+        video.onloadedmetadata = function(e) {
+            console.log("metadata loaded");
+        }
+    }
+
+    navigator.getUserMedia({video:true}, success, failure);
+}
+
+video.addEventListener('click', function() {
+    var width = this.videoWidth;
+    var height = this.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+    // draw webcam picture in canvas
+    ctx.drawImage(video, 0, 0);
+    // create data URL and insert into <img/>
+    img.src = canvas.toDataURL('image/png');
+    // download picture when clicking on it
+    img.onclick = function() {
+        // set filename
+        link.setAttribute('download', 'webcam-'+location.hostname+'-'+Date.now()+'.png');
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+    };
+}, false);
