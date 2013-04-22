7b9caa0a4397accc40d811aaa7fc09271e1add59
Alexander Karlstad <alexander.karlstad@gmail.com>
2013-04-22 13:13:15 +0200

    Made webcam.js user friendly and made code a bit lightweight.

--- webcam.js
+++ webcam.js
@@ -1,58 +1,30 @@
-var video = document.querySelector('video');
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-var img = document.querySelector('img');
-var link = document.createElement('a');
-var hasUserMedia = function() {
-    if (!navigator.getUserMedia) {
-        navigator.getUserMedia = navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    }
-    if (!window.URL) {
-        window.URL = window.webkitURL || window.mozURL;
-    }
-    return !!(navigator.getUserMedia);
-}
+var webcam = function(conf) {
+  var video = conf.video || false;
+  var canvas = document.createElement('canvas');
+  var c2d = canvas.getContext('2d') || false;
+  var callback = conf.callback || false;
+  var failure = conf.failure || function(){};
 
-if (hasUserMedia()) {
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia || false;
+  window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL || false;
+
+  if (video && callback) {
     var failure = function(e) {
-        console.log("Faaaaaaaail", e);
+      failure(e);
     };
 
     var success = function(stream) {
-        if (/Chrome/.test(navigator.userAgent))
-          video.src = window.URL.createObjectURL(stream);
-        else
-          video.src = stream;
+      if (video.mozSrcObject !== undefined) {
+        video.mozSrcObject = stream;
+      }
+      else {
+        video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+      }
 
-        // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
-        // See crbug.com/110938.
-        video.onloadedmetadata = function(e) {
-            console.log("metadata loaded");
-        }
+      callback(video, canvas, stream);
     }
 
-    navigator.getUserMedia({video:true}, success, failure);
-
-    video.addEventListener('click', function() {
-        var width = this.videoWidth;
-        var height = this.videoHeight;
-        canvas.width = width;
-        canvas.height = height;
-        // draw webcam picture in canvas
-        ctx.drawImage(video, 0, 0);
-        // create data URL and insert into <img/>
-        img.src = canvas.toDataURL('image/png');
-        // download picture when clicking on it
-        img.onclick = function() {
-            // set filename for downloading picture
-            // https://developer.mozilla.org/en-US/docs/HTML/Element/a#attr-download
-            link.setAttribute('download', 'webcam-'+location.hostname+'-'+Date.now()+'.png');
-            link.href = canvas.toDataURL('image/png');
-            link.click();
-        };
-    }, false);
-}
-else {
-    alert("your browser seems to be out of date and does not support the use of navigator.getUserMedia()");
+    navigator.getUserMedia({'video':true}, success, failure);
+  }
 }
