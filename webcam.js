71e1d631f229452de24cb8b3d669ef3464599f34
Alexander Karlstad <alexander.karlstad@gmail.com>
2013-05-28 18:10:48 +0200

    Added webcam.js to this branch as well since inclusion from raw.github.com didn't work that well.

---
+++ webcam.js
@@ -0,0 +1,33 @@
+var webcam = function(cbs, cbf, cbns) {
+  var video = document.createElement('video');
+  var canvas = document.createElement('canvas');
+  var c2d = canvas.getContext('2d') || false;
+  var cbf = (typeof cbf == 'function') && cbf || function(){};
+  var cbns = (typeof cbns == 'function') && cbns || function(){};
+
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia || false;
+  window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL || false;
+
+  if (typeof cbs == 'function' && navigator.getUserMedia && window.URL) {
+    var failure = function(e) {
+      cbf(e);
+    };
+
+    var success = function(stream) {
+      if (video.mozSrcObject !== undefined) {
+        video.mozSrcObject = stream;
+      }
+      else {
+        video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+      }
+
+      cbs(video, canvas, stream);
+    }
+
+    navigator.getUserMedia({'video':true}, success, failure);
+  }
+  else {
+    cbns();
+  }
+}
